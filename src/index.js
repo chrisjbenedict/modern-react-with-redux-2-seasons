5fed157ac4705849a93595a0f717519bcd6780ee
github username <chrisjbenedict@gmail.com>
2019-04-26 14:07:14 -0400

    refactored renderContent

--- src/index.js
+++ src/index.js
@@ -19,17 +19,24 @@ class App extends React.Component {
     );
   }
 
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat}/>
+    }
+
+    return <Spinner message="Please accept location request."/>
+  }
+
   render() {
-
-      if (this.state.errorMessage && !this.state.lat) {
-        return <div>Error: {this.state.errorMessage}</div>
-      }
-
-      if (!this.state.errorMessage && this.state.lat) {
-        return <SeasonDisplay lat={this.state.lat}/>
-      }
-
-      return <Spinner />
+    return (
+      <div className='border red'>
+        {this.renderContent()}
+      </div>
+    )
   }
 }
 
